20c8d5de5ed321fe1fd9cbb392d209a7d8e62849
Ayudem <bastienduval1@gmail.com>
2018-05-17 17:28:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,29 +17,37 @@ bot.on('message', (user, userID, channelID, message, evt) => {
     if (userID === bot.id) {
         return
     }
-    notJustTheMen(message)
-    myEnemy(message)
+    const details = {
+        message : message,
+        user : user,
+        userID : userID,
+        channelID : channelID,
+        evt : evt
+    }
+    notJustTheMen(details)
+    myEnemy(details)
 })
 
-function notJustTheMen(message) {
+const notJustTheMen = (details) => {
     const found = message.match(/[A-Za-zÀ-ÿ0-9_]*men[A-Za-zÀ-ÿ0-9_]*/i)
     if (found) {
-        logger.debug(`Triggered by ${user}'s message: ${message}`)
+        logger.debug(`Triggered by ${details.user}'s message: ${details.message}`)
         const men = found[0]
         const women = men.replace(/men/, "women")
         const children = men.replace(/men/, "children")
         bot.sendMessage({
-            to: channelID,
+            to: details.channelID,
             message: `Not just the ${men}, but the ${women} and ${children} too`
         })
     }
 }
 
-function myEnemy(message) {
-    const found = message.match(\bme\b)
+const myEnemy = (details) => {
+    const found = details.message.match(\bme\b)
     if (found) {
+        logger.debug(`Triggered by ${details.user}'s message: ${details.message}`)
         bot.sendMessage({
-            to: channelID,
+            to: details.channelID,
             message: `If you're not with me, then you're my enemy.`
         })
     }
